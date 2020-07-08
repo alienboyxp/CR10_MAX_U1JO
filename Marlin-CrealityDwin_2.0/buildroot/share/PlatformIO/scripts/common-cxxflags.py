89a167c9a483cda819979a1ea5dcc2bda42421b0
Juanjo Rodriguez <juanjoser@asg.com>
2020-07-08 18:56:12 +0200

    Creacion repo

---
+++ Marlin-CrealityDwin_2.0/buildroot/share/PlatformIO/scripts/common-cxxflags.py
@@ -0,0 +1,16 @@
+#
+# common-cxxflags.py
+# Convenience script to apply customizations to CPP flags
+#
+Import("env")
+env.Append(CXXFLAGS=[
+  "-Wno-register"
+  #"-Wno-incompatible-pointer-types",
+  #"-Wno-unused-const-variable",
+  #"-Wno-maybe-uninitialized",
+  #"-Wno-sign-compare"
+])
+
+env_name = str(env["PIOENV"])
+env.Replace(PROGNAME="%s_DW6.2" % (env_name))
+print("Environment: %s" % (env_name))
